8d7d0e33a6e6ab861f7fa9e07fc05f960d0aafab
pedrohelias <pedrohelias95@hotmail.com>
2024-07-02 16:50:19 -0300

    atualizando para testes parametrizados.

--- tppe-varejo/src/Imposto.java
+++ tppe-varejo/src/Imposto.java
@@ -1,6 +1,15 @@
 public class Imposto {
+
+    public String regiao;
+    public double valorCompra;
     
-    public static double ICMS(String regiao, double valorCompra){
+    public Imposto(String regiao, double valorCompra) {
+        //TODO Auto-generated constructor stub
+        this.regiao = regiao;
+        this.valorCompra = valorCompra;
+    }
+
+    public double ICMS(String regiao, double valorCompra){
         if(regiao == "DF"){
             return valorCompra * 0.18;
         }else{
--- tppe-varejo/src/ImpostoTest.java
+++ tppe-varejo/src/ImpostoTest.java
@@ -1,48 +1,80 @@
+
+
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+@RunWith(Parameterized.class)
 public class ImpostoTest {
 
-    //referencia
-    //12% de ICMS e 4% de imposto municipal em vendas para clientes fora do DF;
-    //18% de ICMS e 0% de imposto municipal em vendas para cliende do DF.
-    
+    private Imposto imp;
+    private String regiao;
+    private double valorCompra;
+    private double valorEsperado;
 
-    @Test
-    public void testICMSDentroDF(){
-        String regiao = "DF";
-        double valorCompra = 100;
-        double obtido = Imposto.ICMS(regiao,valorCompra);
-        assertEquals((valorCompra * 0.18) , obtido, .1);
+    @Parameters
+    public static Collection<Object[]> getParameters(){
+        return Arrays.asList(new Object[][] {
+                {"DF", 100, 18}, //dentro do DF
+                {"DF", 200, 36},
+                {"SUL", 100, 12} //fora do DF
+        });
+    }
 
+    public ImpostoTest(String regiao, double valorCompra, double valorEsperado){
+        this.regiao = regiao;
+        this.valorCompra = valorCompra;
+        this.valorEsperado = valorEsperado;
     }
 
     @Test
-    public void testICMSForaDF(){
-        String regiao = "SUL";
-        double valorCompra = 100;
-        double obtido = Imposto.ICMS(regiao,valorCompra);
-        assertEquals((valorCompra * 0.12) , obtido, .1);
-
+    public void testICMS(){
+        imp = new Imposto(regiao, valorCompra);
+        assertEquals(valorEsperado, imp.ICMS(regiao, valorCompra), 0.1);
     }
-
-    @Test
-    public void testImpMunicipalDentroDF(){
-        String regiao = "DF";
-        double valorCompra = 100;
-        double obtido = Imposto.ImpMunicipal(regiao,valorCompra);
-        assertEquals((valorCompra) , obtido, .1);
-
-    }
-
-    @Test
-    public void testImpMunicipalForaDF(){
-        String regiao = "NORDESTE";
-        double valorCompra = 100;
-        double obtido = Imposto.ImpMunicipal(regiao,valorCompra);
-        assertEquals((valorCompra * 0.04) , obtido, .1);
-
-    }
-
 }
+
+    // @Test
+    // public void testICMSDentroDF(){
+    //     String regiao = "DF";
+    //     double valorCompra = 100;
+    //     double obtido = Imposto.ICMS(regiao,valorCompra);
+    //     assertEquals((valorCompra * 0.18) , obtido, .1);
+
+    // }
+
+    // @Test
+    // public void testICMSForaDF(){
+    //     String regiao = "SUL";
+    //     double valorCompra = 100;
+    //     double obtido = Imposto.ICMS(regiao,valorCompra);
+    //     assertEquals((valorCompra * 0.12) , obtido, .1);
+
+    // }
+
+    // @Test
+    // public void testImpMunicipalDentroDF(){
+    //     String regiao = "DF";
+    //     double valorCompra = 100;
+    //     double obtido = Imposto.ImpMunicipal(regiao,valorCompra);
+    //     assertEquals((valorCompra) , obtido, .1);
+
+    // }
+
+    // @Test
+    // public void testImpMunicipalForaDF(){
+    //     String regiao = "NORDESTE";
+    //     double valorCompra = 100;
+    //     double obtido = Imposto.ImpMunicipal(regiao,valorCompra);
+    //     assertEquals((valorCompra * 0.04) , obtido, .1);
+
+    // }
+
+
